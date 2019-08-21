35512303b5bb2479ee84aa2349d37161c1d1a72f
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-08-21 12:05:20 -0700

    Created basic outline of django template for the front end of the  website

--- fergusonart/settings.py
+++ fergusonart/settings.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = 'fergusonart.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join[BASE_DIR, 'gallery', 'layout/']],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
